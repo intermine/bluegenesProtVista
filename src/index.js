e9e24d746d8d786b89fba23485c091f6f05cdec3
uosl <regen@anche.no>
2020-07-10 16:38:27 +0100

    Support Tool API v2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ProtVista from "ProtVista";
 
-export function main (el, service, imEntity, state, config) {
+export function main (el, service, entity, state, config) {
     //protVista expects an accession, so convert intermine id to accession
     //TODO: How do we handle multiple values, e.g. list ids?
+    var imEntity = entity.Protein;
     var columnToConvert = config.columnMapping[imEntity.class][imEntity.format];
     var accession = new imjs.Service(service)
         .findById(imEntity.class, imEntity.value)
